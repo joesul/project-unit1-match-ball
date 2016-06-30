0fcebce2e92b21cddfd99d95eedeb7249f8ff8f3
joseph sul <joesul7@gmail.com>
2016-06-30 14:23:27 -0400

    updated files

---
+++ main.js
@@ -0,0 +1,36 @@
+console.log("main.js has loaded...");
+document.addEventListener("DOMContentLoaded", start());
+
+var p1winCount = 0;
+var p2winCount = 0;
+
+function start () {
+  roundStart();
+}
+
+function roundStart () {
+  var roundCount = 0;
+  roundCount++;
+
+  console.log(roundCount)
+
+  window.addEventListener("keydown", player1);
+  window.addEventListener("keydown", player2);
+
+  function player1(e) {
+    var p1ball = document.querySelector("#p1ball");
+      if (e.keyCode == "65") {
+      p1ball.classList.add("speed-one");
+      console.log("level one");
+      }
+    }
+
+
+  function player2(e) {
+    var p2ball = document.querySelector("#p2ball");
+    if (e.keyCode == "76") {
+      p2ball.classList.add("level one");
+      }
+    }
+
+}
