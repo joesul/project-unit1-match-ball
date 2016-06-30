a411193f0dee9c8cd0467aab6fa3c85158a28ea7
joseph sul <joesul7@gmail.com>
2016-06-30 14:40:58 -0400

    made level one

--- main.js
+++ main.js
@@ -16,21 +16,21 @@ function roundStart () {
 
   window.addEventListener("keydown", player1);
   window.addEventListener("keydown", player2);
+  if (roundCount == 1) {
+    function player1(e) {
+      var p1ball = document.querySelector("#p1ball");
+        if (e.keyCode == "65") {
+        p1ball.classList.add("speed-one");
+        console.log("level one");
+        }
+      }
 
-  function player1(e) {
-    var p1ball = document.querySelector("#p1ball");
-      if (e.keyCode == "65") {
-      p1ball.classList.add("speed-one");
-      console.log("level one");
+
+    function player2(e) {
+      var p2ball = document.querySelector("#p2ball");
+      if (e.keyCode == "76") {
+        p2ball.classList.add("level one");
+        }
       }
     }
-
-
-  function player2(e) {
-    var p2ball = document.querySelector("#p2ball");
-    if (e.keyCode == "76") {
-      p2ball.classList.add("level one");
-      }
-    }
-
 }
